18fb0073a8c5cefc9ce153e17b5e13681efb19b5
Luke Macken <lmacken@redhat.com>
2013-07-03 14:34:48 -0400

    Fix another syntax error

--- wsgi/corniceapp/corniceapp/views.py
+++ wsgi/corniceapp/corniceapp/views.py
@@ -9,5 +9,5 @@ hello = Service(name='hello', path='/', description="Simplest app")
 @hello.get()
 def get_info(request):
     """Returns Hello in JSON."""
-    return {'Hello': 'World'
+    return {'Hello': 'World',
             'Open': 'Shift'}
